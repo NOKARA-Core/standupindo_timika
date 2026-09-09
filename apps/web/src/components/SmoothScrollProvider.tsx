ce48309ea55f5728b91f220484afddfa08352055
dayattt111 <tumbaldaya@gmail.com>
2026-09-09 17:24:15 +0900

    feat: implement smooth scrolling using Lenis provider

---
+++ apps/web/src/components/SmoothScrollProvider.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export default function SmoothScrollProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.1, // Durasi scroll pas, tidak terasa 'licin' berlebihan
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Kurva akselerasi natural
+      smoothWheel: true,
+      touchMultiplier: 1.5,
+      infinite: false,
+    });
+
+    let rafId: number;
+
+    function raf(time: number) {
+      lenis.raf(time);
+      rafId = requestAnimationFrame(raf);
+    }
+
+    rafId = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(rafId);
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}
